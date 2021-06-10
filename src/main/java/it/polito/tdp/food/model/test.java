245abdbb5ae7720a046411d2ed914350309f0ae6
Lorenzo <s259524@studenti.polito.it>
2021-06-10 18:48:27 +0200

    h

---
+++ src/main/java/it/polito/tdp/food/model/test.java
@@ -0,0 +1,11 @@
+package it.polito.tdp.food.model;
+
+public class test {
+
+	public static void main(String[] args) {
+Model m=new Model();
+String s=m.creaGrafo(1);
+System.out.println(s);
+	}
+
+}
